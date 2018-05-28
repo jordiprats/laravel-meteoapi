7d4f185a4d451f4ad2f68fbff8eed194b999e57d
jordiprats <jordi.prats@gmail.com>
2018-05-29 01:01:58 +0200

    notes

--- meteoapi/app/Http/Controllers/PrevisioController.php
+++ meteoapi/app/Http/Controllers/PrevisioController.php
@@ -6,5 +6,6 @@ use Illuminate\Http\Request;
 
 class PrevisioController extends Controller
 {
-  //
+  // http://meteo.cat/prediccio/platges/tossa-de-mar-de-la-mar-menuda
+  // preg_match('/dades: (.*),/', $html, $matches, PREG_OFFSET_CAPTURE);
 }
