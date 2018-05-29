34e15e380e11635dc8205c572492ef3d2f850209
jordiprats <jordi.prats@gmail.com>
2018-05-29 23:31:35 +0200

    skel basic ftch previsio platja

--- meteoapi/app/Http/Controllers/PrevisioController.php
+++ meteoapi/app/Http/Controllers/PrevisioController.php
@@ -6,6 +6,27 @@ use Illuminate\Http\Request;
 
 class PrevisioController extends Controller
 {
-  // http://meteo.cat/prediccio/platges/tossa-de-mar-de-la-mar-menuda
-  // preg_match('/dades: (.*),/', $html, $matches, PREG_OFFSET_CAPTURE);
+  public static function getPrevisioPlatja($platja_slug)
+  {
+    // http://meteo.cat/prediccio/platges/tossa-de-mar-de-la-mar-menuda
+    $c = curl_init('http://meteo.cat/prediccio/platges/'.$platja_slug);
+    curl_setopt($c, CURLOPT_RETURNTRANSFER, true);
+    curl_setopt($c, CURLOPT_USERAGENT,'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:59.0) Gecko/20100101 Firefox/59.0');
+    //curl_setopt(... other options you want...)
+
+    $html = curl_exec($c);
+
+    if (curl_error($c))
+        die(curl_error($c));
+
+    // Get the status code
+    $status = curl_getinfo($c, CURLINFO_HTTP_CODE);
+
+    curl_close($c);
+
+    // preg_match('/dades: (.*),/', $html, $matches, PREG_OFFSET_CAPTURE);
+    preg_match('/dades: (.*),/', $html, $matches, PREG_OFFSET_CAPTURE);
+
+    return json_decode($matches[1][0]);
+  }
 }
