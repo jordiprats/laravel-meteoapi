239912083fbd906f90852fef848bc2430bc0b6f8
jordiprats <jordi.prats@gmail.com>
2018-06-10 22:43:11 +0200

    estructura previsió

--- meteoapi/app/Jobs/GetPrevisioMunicipal.php
+++ meteoapi/app/Jobs/GetPrevisioMunicipal.php
@@ -40,7 +40,7 @@ class GetPrevisioMunicipal implements ShouldQueue
 
     if($municipi)
     {
-      $previsions = PrevisioController::getPrevisioMunicipal($municipi->id);
+      $fetched_previsio = PrevisioController::getPrevisioMunicipal($municipi->id);
 
       // TODO: data_previsio
       // [1] => stdClass Object
@@ -66,17 +66,7 @@ class GetPrevisioMunicipal implements ShouldQueue
         'DG' => 'sun',
       ];
 
-      Log::info(print_r($previsions));
-      foreach ($previsions as $fetched_previsio)
-      {
-        // echo gmdate('F j, Y', strtotime('next fri'));
-
-        //var_dump($fetched_previsio->dies);
-
-        //new \DateTime(
-
-        //$previsio = Previsio::where(['municipi_id' => $municipi->id, 'data_previsio' => new \DateTime($fetched_previsio->data)])->first();
-      }
+      var_dump($fetched_previsio->dies);
     }
     else
       Log::info("municipi NOT FOUND: ".$this->municipi_slug);
