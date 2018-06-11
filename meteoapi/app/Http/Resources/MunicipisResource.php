b207e0ff0927f16993dec9ce4ab11810e92aa89b
jordiprats <jordi.prats@gmail.com>
2018-06-11 07:08:39 +0200

    withoutWrapping

--- meteoapi/app/Http/Resources/MunicipisResource.php
+++ meteoapi/app/Http/Resources/MunicipisResource.php
@@ -6,16 +6,17 @@ use Illuminate\Http\Resources\Json\ResourceCollection;
 
 class MunicipisResource extends ResourceCollection
 {
-    /**
-     * Transform the resource collection into an array.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return array
-     */
-    public function toArray($request)
-    {
-        return [
-          'municipis' => MunicipiResource::collection($this->collection),
-        ];
-    }
+  /**
+   * Transform the resource collection into an array.
+   *
+   * @param  \Illuminate\Http\Request  $request
+   * @return array
+   */
+  public function toArray($request)
+  {
+    MunicipisResource::withoutWrapping();
+    return [
+      'municipis' => MunicipiResource::collection($this->collection),
+    ];
+  }
 }
