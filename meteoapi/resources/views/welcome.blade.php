a3482191698d36ba62dff3df37f6d339c163bfb9
jordiprats <jordi.prats@gmail.com>
2018-06-13 07:54:14 +0200

    welcome

--- meteoapi/resources/views/welcome.blade.php
+++ meteoapi/resources/views/welcome.blade.php
@@ -79,7 +79,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    meteoapi - República de Catalunya
                 </div>
 
                 <div class="links">
