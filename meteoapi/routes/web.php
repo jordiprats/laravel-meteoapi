257a877639281ebb7492a6b1cd0eff7805c670b9
jordiprats <jordi.prats@gmail.com>
2018-06-16 12:24:21 +0200

    nom ruta duplicat

--- meteoapi/routes/web.php
+++ meteoapi/routes/web.php
@@ -22,7 +22,7 @@ Route::get('/municipis', 'MunicipiController@index')->name('municipis.list');
 Route::get('/municipis/{municipi_slug}/previsio', 'MunicipiController@previsio')->name('municipis.previsio');
 Route::get('/municipis/{municipi_slug}/platges', 'MunicipiController@platges')->name('municipis.platges');
 Route::get('/municipis/{municipi_slug}/platges/{platja_slug}', 'PlatjaController@show')->name('platges.show');
-Route::get('/municipis/{municipi_slug}/platges/{platja_slug}/previsio', 'PlatjaController@previsio')->name('platges.show');
+Route::get('/municipis/{municipi_slug}/platges/{platja_slug}/previsio', 'PlatjaController@previsio')->name('platges.show.previsio');
 
 Route::get('/comarques', 'ComarcaController@index')->name('comarques.list');
 Route::get('/comarques/{comarca_slug}', 'ComarcaController@show')->name('comarca.show');
